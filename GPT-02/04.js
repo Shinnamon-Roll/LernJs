b9cf49b47a2c6f465cbe823092c3d38926d7aa52
Shinnamon-Roll <chimonkung0@gmail.com>
2025-06-08 21:27:39 +0700

    wtf async

---
+++ GPT-02/04.js
@@ -0,0 +1,9 @@
+async function testNumber() {
+    const num = await new Promise((resolve, reject) => {
+        let testNumber = Math.floor(Math.random() * 10) + 1
+        resolve(num)
+    }) // สุ่มเลข
+    console.log(result)
+}
+
+testNumber()
