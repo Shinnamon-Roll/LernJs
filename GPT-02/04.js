4431ccfaf794fb649c9fb0cbf96a2ecf569c1055
Shinnamon-Roll <chimonkung0@gmail.com>
2025-06-08 21:43:44 +0700

    understand 100% await

--- GPT-02/04.js
+++ GPT-02/04.js
@@ -1,9 +1,8 @@
-async function testNumber() {
-    const num = await new Promise((resolve, reject) => {
-        let testNumber = Math.floor(Math.random() * 10) + 1
-        resolve(num)
-    }) // สุ่มเลข
-    console.log(result)
+async function getPost() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+    const post = await response.json()
+    console.log(post)
+
 }
 
-testNumber()
+getPost()
